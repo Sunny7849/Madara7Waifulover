ec964b69ad2fe6f952cf78391da6050b9b1350bd
Sunny7849 <164983493+Sunny7849@users.noreply.github.com>
2025-03-24 14:03:42 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -2,23 +2,23 @@ class Config(object):
     LOGGER = True
 
     # Get this value from my.telegram.org/apps
-    OWNER_ID = "6765826972"
-    sudo_users = "6845325416", "6765826972"
-    GROUP_ID = -1002133191051
-    TOKEN = "6707490163:AAHZzqjm3rbEZsObRiNaT7DMtw_i5WPo_0o"
-    mongo_url = "mongodb+srv://HaremDBBot:ThisIsPasswordForHaremDB@haremdb.swzjngj.mongodb.net/?retryWrites=true&w=majority"
+    OWNER_ID = "7885908019"
+    sudo_users = ("7756901810", "8043832960")  # Tuple me proper format
+    GROUP_ID = -1002372914024
+    TOKEN = "7689391231:AAEHXLz_uEv4Kpd-pZHQqVjkPujA7-ObJZw"
+    mongo_url = "mongodb+srv://Sunnydeol:TGDARK0987@cluster0.fsb3s.mongodb.net/?retryWrites=true&w=majority"
     PHOTO_URL = ["https://telegra.ph/file/b925c3985f0f325e62e17.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
-    SUPPORT_CHAT = "Collect_em_support"
-    UPDATE_CHAT = "Collect_em_support"
-    BOT_USERNAME = "Collect_Em_AllBot"
-    CHARA_CHANNEL_ID = "-1002133191051"
-    api_id = 26626068
-    api_hash = "bf423698bcbe33cfd58b11c78c42caa2"
+    SUPPORT_CHAT = "https://t.me/+ZTeO__YsQoIwNTVl"
+    UPDATE_CHAT = "https://t.me/Anime_P_F_P"
+    BOT_USERNAME = "Waifu_ChanBbot"
+    CHARA_CHANNEL_ID = "-1002640379822"
+    api_id = 28159105
+    api_hash = "a0936ddf210a7e091e19947c7dc70c91"
 
     
 class Production(Config):
     LOGGER = True
 
 
-class Development(Config):
+class Development(Config):  # ✅ Colon laga diya
     LOGGER = True
