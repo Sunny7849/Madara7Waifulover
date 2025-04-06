0b0490f523147fd4c9cde11f7529c45620a81ab1
Sunny7849 <164983493+Sunny7849@users.noreply.github.com>
2025-04-06 11:35:52 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -2,15 +2,15 @@ class Config(object):
     LOGGER = True
 
     # Get this value from my.telegram.org/apps
-    OWNER_ID = "7885908019"
+    OWNER_ID = "8156600797"
     sudo_users = ("7756901810", "8043832960")  # Tuple me proper format
     GROUP_ID = -1002372914024
-    TOKEN = "7689391231:AAEHXLz_uEv4Kpd-pZHQqVjkPujA7-ObJZw"
-    mongo_url = "mongodb+srv://Sunnydeol:TGDARK0987@cluster0.fsb3s.mongodb.net/?retryWrites=true&w=majority"
+    TOKEN = "7539465396:AAFT5I6oK0wRJHSFNaAUMosQ4uFm2pHa7_c"
+    mongo_url = "mongodb+srv://Narutoo:hinatahyuga@hater.0luw79s.mongodb.net/"
     PHOTO_URL = ["https://telegra.ph/file/b925c3985f0f325e62e17.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
     SUPPORT_CHAT = "https://t.me/+ZTeO__YsQoIwNTVl"
     UPDATE_CHAT = "https://t.me/Anime_P_F_P"
-    BOT_USERNAME = "Waifu_ChanBbot"
+    BOT_USERNAME = "Daddy_Madara_WaifuBot"
     CHARA_CHANNEL_ID = "-1002640379822"
     api_id = 28159105
     api_hash = "a0936ddf210a7e091e19947c7dc70c91"
